c0bf954bc78fd10a36ea31b480342dcfc7e52c94
David-Happel <davidvhappel1@gmail.com>
2021-11-16 15:12:43 +0100

    reading data windowed

--- read_data.py
+++ read_data.py
@@ -2,14 +2,22 @@ import rasterio
 from rasterio.windows import Window
 from matplotlib import pyplot
 
-win = Window(0, 0, 250, 250)    
 #open file    
 with rasterio.open("data/deepcrop/tiles/X0071_Y0043/2018-2018_001-365_HL_TSA_SEN2L_GRN_TSI.tif") as src:
     print(src.count, src.width, src.height)  
     pyplot.imshow(src.read(1), cmap='pink')
-    pyplot.show()
-    w = src.read(1, window=win)
-    pyplot.imshow(w, cmap='pink')
+    pyplot.figure()
+
+    w = src.read(1, window=Window(0, 0, 224, 224))
+    pyplot.imshow(w , cmap='pink')
+    pyplot.figure()
+
+    w = src.read(1, window=Window(200, 0, 224, 224))
+    pyplot.imshow(w , cmap='pink')
+    pyplot.figure()
+
+    w = src.read(1, window=Window(400, 0, 224, 224))
+    pyplot.imshow(w , cmap='pink')
     pyplot.show()
 
 #write meta data    
