fdbcd14f270d96cb57ce563dcf9323d4be11a8a4
David-Happel <davidvhappel1@gmail.com>
2021-11-17 11:34:20 +0100

    Main reading loop

--- read_data.py
+++ read_data.py
@@ -1,36 +1,36 @@
 import rasterio
+import numpy as np
 from rasterio.windows import Window
 from matplotlib import pyplot
 
-#open file    
-with rasterio.open("data/deepcrop/tiles/X0071_Y0043/2018-2018_001-365_HL_TSA_SEN2L_GRN_TSI.tif") as src:
-    print(src.count, src.width, src.height, src.crs)  
-    pyplot.imshow(src.read(1), cmap='pink')
-    pyplot.figure()
-    pyplot.imshow(src.read(10), cmap='pink')
-    pyplot.figure()
-    pyplot.imshow(src.read(20), cmap='pink')
-    pyplot.show()
+imageWidth = 224
+imageHeight = 224
 
-    # w = src.read(1, window=Window(0, 0, 224, 224))
-    # pyplot.imshow(w , cmap='pink')
-    # pyplot.figure()
+bands = ["GRN", "NIR"]
 
-    # w = src.read(1, window=Window(200, 0, 224, 224))
-    # pyplot.imshow(w , cmap='pink')
-    # pyplot.figure()
+for band in bands:
+    print("Band:", band)
+    #open file    
+    with rasterio.open(f'data/deepcrop/tiles/X0071_Y0043/2018-2018_001-365_HL_TSA_SEN2L_{band}_TSI.tif') as src:
+        print(src.count, src.width, src.height, src.crs) 
+        for time in range(1, src.count+1):
+            print("Time:", time)
+            for x in np.arange(0, src.width, 200):
+                for y in np.arange(0, src.height, 200):
+                    print("Reading Window:", x,y)
+                    window = Window(x,y,imageWidth, imageHeight)
+                    w = src.read(time, window=window)
 
-    # w = src.read(1, window=Window(400, 0, 224, 224))
-    # pyplot.imshow(w , cmap='pink')
-    # pyplot.show()
+                    # write meta data    
+                    xform = rasterio.windows.transform(window, src.meta['transform'])    
+                    meta_d=src.meta.copy()    
+                    meta_d.update({"height": imageWidth,
+                                "width": imageHeight,
+                                "transform": xform})  
 
-#write meta data    
-# xform = rasterio.windows.transform(win, src.meta['transform'])    
-# meta_d=src.meta.copy()    
-# meta_d.update({"height": height,
-#                "width": width,
-#                "transform": xform})  
+                    # # write output    
+                    # with rasterio.open(outfile, "w", **meta_d) as dest:                           
+                    #     dest.write_band(band, w)
+
+
   
-#write output    
-# with rasterio.open(outfile, "w", **meta_d) as dest:                           
-#     dest.write_band(1, w)
