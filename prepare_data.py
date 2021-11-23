503698810acb8f785f63b08ffba57f36137a440e
Stephen Moran <stephenmoran@Stephens-MacBook-Pro.local>
2021-11-23 16:08:26 +0100

    csv file for every badn added

--- prepare_data.py
+++ prepare_data.py
@@ -21,9 +21,10 @@ data_filename = '2018-2018_001-365_HL_TSA_SEN2L_{band}_TSI.tiff'
 
 out_dir = "data/prepared/"
 out_filename = '{sample}_{band}.tif'
-
 Path(out_dir).mkdir(parents=True, exist_ok=True)
-"""
+#empty array to store new window image ids
+ids = []
+
 for band in bands:
     print("Band:", band)
     #open file    
@@ -45,13 +46,13 @@ for band in bands:
                 meta_d.update({"height": imageWidth,
                             "width": imageHeight,
                             "transform": xform})
-                            
+                ids.append(out_filename.format(sample= sample_i, band = band))           
                 outfile_path = out_dir+out_filename.format(sample= sample_i, band = band)
                 with rasterio.open(outfile_path, "w", band=band, **meta_d) as dest:
                     dest.write_band(times, w)
 
                 sample_i += 1
-"""
+
 
 # Class labels
 file_path = 'data/deepcrop/tiles/X0071_Y0043/IACS_2018.tiff'
@@ -71,27 +72,34 @@ with rasterio.open(file_path) as src:
             #print("Reading Window:", x,y)
             window = Window(x,y,imageWidth, imageHeight)
             w = src.read(1, window=window)
-
-            # write meta data 
-                
-            #xform = rasterio.windows.transform(window, src.meta['transform'])    
-            #meta_d=src.meta.copy()    
-            #meta_d.update({"height": imageWidth,
-               #         "width": imageHeight,
-                #        "transform": xform})
-            #print(np.unique(w))
             labels.append(list(np.unique(w)))
+
             print(list(np.unique(w)))
 
 
-print(len(labels))
+#create entry for every image band
+copy = labels 
+for i in range(len(bands) - 1):
+    labels += copy
 
-d = {'labels': labels}
+print(len(labels))
+print(len(ids))
+
+d = {'image_id': ids, 'labels': labels}
 df = pd.DataFrame(data=d)        
 print(df.head(n=5))
+print(df.loc[df['image_id'] == '0_NIR.tif'])
+
+#image ids dataframe
+image_ids = df["image_id"].to_frame()
+#d = {'image_id': df["image_id"]}
+#df_i = pd.DataFrame(data=d)  
 
 mlb = MultiLabelBinarizer()
-df = pd.DataFrame(mlb.fit_transform(df['labels']),columns=[0, 10, 31, 32, 33, 34, 41, 42, 43, 50, 60, 70, 91, 92, 100, 120, 140, 181, 182])
-df.to_csv(out_dir + "labels.csv", index=True)
+df1 = pd.DataFrame(mlb.fit_transform(df['labels']),columns=[0, 10, 31, 32, 33, 34, 41, 42, 43, 50, 60, 70, 91, 92, 100, 120, 140, 181, 182])
+#df1 = df1.join(image_ids)
+df1 = image_ids.join(df1)
+df1.to_csv(out_dir + "labels.csv", index=True)
 
- 
+
+print(df1.head(n=5))
