8e64491952f6bc9be6254afd215458f645cdd4bf
David Happel <davidvhappel1@gmail.com>
2021-12-10 10:55:13 +0100

    gpu_short partition

--- run_train.sh
+++ run_train.sh
@@ -3,9 +3,9 @@
 #SBATCH -J test-distributed-training
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:tesla:2
+#SBATCH --gres=gpu:tesla:1
 #SBATCH --mem=50G
-#SBATCH --partition=standard
+#SBATCH --partition=gpu_short
 
 source /home/users/d/davidhappel/venv/cv4rs/bin/activate
 module load nvidia/cuda/10.0
