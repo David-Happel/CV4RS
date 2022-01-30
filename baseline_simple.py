07d6466a7c0a3af4fb4aa96157a76127b0e17ceb
David Happel <davidvhappel1@gmail.com>
2022-01-30 15:16:45 +0100

    fix parameter name

--- baseline_simple.py
+++ baseline_simple.py
@@ -35,7 +35,7 @@ class C3D(nn.Module):
     ================================================================
     """
 
-    def __init__(self, bands=3, labels=24, times=6, device="cpu"):
+    def __init__(self, bands=3, labels=24, time=6, device="cpu"):
         super(C3D, self).__init__()
 
         self.conv1 = nn.Conv3d(bands, 32, kernel_size=(3, 3, 3), padding=(1, 1, 1))
--- main.py
+++ main.py
@@ -142,7 +142,7 @@ def main():
                         batch_size=batch_size, sampler=test_subsampler)
 
         #model selection
-        model = model_class(bands=len(bands), labels=len(class_weights), times=6, device=device).to(device)
+        model = model_class(bands=len(bands), labels=len(class_weights), time=6, device=device).to(device)
 
         optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
