ead0516f155ea7c238bff68effd0fad965b7a3f6
David Happel <davidvhappel1@gmail.com>
2022-01-30 15:01:18 +0100

    update scripts

--- lstm_6t15b.sh
+++ lstm_6t15b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -o /home/users/d/davidhappel/CV4RS/6t10b.out
+#SBATCH -o /home/users/d/davidhappel/CV4RS/lstm_6t15b.out
 #SBATCH --chdir=/home/users/d/davidhappel/CV4RS
 #SBATCH -J CV4RS
 #SBATCH --ntasks=2
@@ -16,4 +16,4 @@ echo "Loading cuda..."
 module load nvidia/cuda/10.1
 
 echo "Executing..."
-python3 main.py --epochs 5 --batch_size 15 --timepoints 6 --model bl --name 6t10b --no_process_data
+python3 main.py --epochs 5 --batch_size 15 --timepoints 6 --model lstm --name lstm_6t15b --no_process_data
--- trans_6t15b.sh
+++ trans_6t15b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -o /home/users/d/davidhappel/CV4RS/18t18b.out
+#SBATCH -o /home/users/d/davidhappel/CV4RS/trans_6t15b.out
 #SBATCH --chdir=/home/users/d/davidhappel/CV4RS
 #SBATCH -J CV4RS
 #SBATCH --ntasks=2
@@ -7,7 +7,7 @@
 #SBATCH --mem=100G
 #SBATCH --partition=gpu_short
 #SBATCH --time=24:00:00
-#SBATCH --gres=gpu:tesla:1
+#SBATCH --gres=gpu:tesla:2
 
 echo "Loading venv..."
 source /home/users/d/davidhappel/venv/cv4rs/bin/activate
@@ -16,4 +16,4 @@ echo "Loading cuda..."
 module load nvidia/cuda/10.1
 
 echo "Executing..."
-python3 main.py --epochs 50 --batch_size 10 --timepoints 18 --model bl --name 18t18b --no_process_data
+python3 main.py --epochs 5 --batch_size 15 --timepoints 6 --model trans --name trans_6t15b --no_process_data
