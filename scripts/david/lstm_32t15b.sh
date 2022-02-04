2d75724b2ac8431ccc4e8124e6e6bd7ece3a9132
David Happel <davidvhappel1@gmail.com>
2022-02-04 13:23:53 +0100

    32 times scripts

---
+++ scripts/david/lstm_32t15b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -o /home/users/d/davidhappel/CV4RS/lstm_32t15b.out
+#SBATCH --chdir=/home/users/d/davidhappel/CV4RS
+#SBATCH -J CV4RS
+#SBATCH --ntasks=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=100G
+#SBATCH --partition=gpu_short
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:tesla:2
+
+echo "Loading venv..."
+source /home/users/d/davidhappel/venv/cv4rs/bin/activate
+
+echo "Loading cuda..."
+module load nvidia/cuda/10.1
+
+echo "Executing..."
+python3 main.py --epochs 5 --batch_size 15 --timepoints 32 --model lstm --name lstm_32t15b --no_process_data
